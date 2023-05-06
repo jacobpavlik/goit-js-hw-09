89e5f3c7ea5ae13368c794c25523a61565d585a7
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-05-07 00:00:04 +0200

    Added task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,45 @@
 import Notiflix from 'notiflix';
+const inputDelay = document.querySelector('[name=delay]');
+const inputStep = document.querySelector('[name=step]');
+const inputAmount = document.querySelector('[name=amount]');
+const createPromisesBtn = document.querySelector('button');
+// const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(() => {
+    if (shouldResolve) {
+      // Fulfill
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      // return Promise.resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      // Reject
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      // return Promise.reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
+  console.log(`delay z setTimout ${delay}`);
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// form.addEventListener('input', () => {
+//   console.log('inputDelay');
+//   console.log(inputDelay.value);
+//   console.log('');
+//   console.log('inputStep');
+//   console.log(inputStep.value);
+//   console.log('');
+//   console.log('inputAmount');
+//   console.log(inputAmount.value);
+//   console.log('');
+// });
+createPromisesBtn.addEventListener('click', event => {
+  event.preventDefault();
+  let delay = inputDelay.value - inputStep.value;
+  for (let i = 0; i < inputAmount.value; i += 1) {
+    position = i;
+    delay = +delay;
+    delay += +inputStep.value;
+    createPromise(i, delay);
+    console.log(`i: ${i}, delay: ${delay}`);
+  }
+});
